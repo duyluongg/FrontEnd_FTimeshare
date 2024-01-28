b657789a5e62a470814908f50406ec36a47760a6
duyluongg <147598253+duyluongg@users.noreply.github.com>
2024-01-28 23:34:02 +0700

    done detail.js

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useState } from "react";
 import { useParams } from 'react-router-dom'
 import { ProjectsData } from '../../Shared/ListOfProject';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 import Slider from "react-slick";
+import { ProjectsDataSimilar } from '../../Shared/ListOfProjectSimilar';
+import { RoomData } from '../../Shared/Room';
+
+
 
 import {
     faPerson, faChild, faExpand, faBath,
@@ -37,12 +43,33 @@ function SamplePrevArrow({ onClick }) {
 
     );
 }
+
+function SampleNextArrowSt2({ onClick }) {
+
+    return (
+        <div className='arrowst arrowst-right' onClick={onClick}>
+            <MdArrowForwardIos />
+        </div>
+
+    );
+}
+
+function SamplePrevArrowSt2({ onClick }) {
+
+    return (
+        <div className='arrowst arrowst-left' onClick={onClick}>
+            <MdArrowBackIosNew />
+        </div>
+
+    );
+}
 export default function Detail() {
     const projectName = useParams();
     const projectId = ProjectsData.find(obj => {
         return obj.id === projectName.id;
     });
     let cost = projectId.cost.toLocaleString();
+    const [activeContentIndex, setActiveContentIndex] = useState(projectId.desribe0);
     const settings = {
         focusOnSelect: true,
         infinite: true,
@@ -51,6 +78,17 @@ export default function Detail() {
         speed: 300,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />
+    };
+
+    const settings2 = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: <SampleNextArrowSt2 />,
+        prevArrow: < SamplePrevArrowSt2 />
+
     };
     return (
         <div className='container-detail'>
@@ -127,10 +165,10 @@ export default function Detail() {
 
                         <form className='form-item'>
                             <div className='column-form column-1'>
-                                <label for="fullName">Họ Tên:</label>
+                                <label for="fullName">First and last name:</label>
                                 <input type="text" id="fullName" name="fullName" required />
 
-                                <label for="phoneNumber">Số Điện Thoại:</label>
+                                <label for="phoneNumber">Phone number:</label>
                                 <input type="tel" id="phoneNumber" name="phoneNumber" pattern="[0-9]{10}" required />
 
                                 <label for="adults">Adult:</label>
@@ -154,6 +192,137 @@ export default function Detail() {
                     </div>
                 </div>
             </div>
+
+            <div id='tabs'>
+                <menu>
+                    <button
+                        className={`custom-button ${activeContentIndex === projectId.desribe0 ? "active" : ""}`}
+                        onClick={() => setActiveContentIndex(projectId.desribe0)}
+
+                    >
+                        Why React?
+                    </button>
+
+                    <button
+                        className={`custom-button ${activeContentIndex === projectId.desribe1 ? "active" : ""}`}
+                        onClick={() => setActiveContentIndex(projectId.desribe1)}
+                    >
+                        Core Features
+                    </button>
+
+
+                </menu>
+
+                <div id="tab-content">
+                    {/* <ul>
+                        {content[activeContentIndex].map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul> */}
+                    <div>
+                        {activeContentIndex === projectId.desribe0 &&
+                            <div>
+
+                                <p>{projectId.desribe0}</p>
+                                <p>Free services:{projectId.describe0service}</p>
+                                <p>Check-out time:{projectId.describe0chkout}</p>
+                                <p>Payment method:{projectId.desribe0pay}</p>
+                            </div>
+
+                        }
+                        {activeContentIndex === projectId.desribe1 && <p>{projectId.desribe1}</p>}
+                    </div>
+
+                </div>
+
+                <div>
+
+                    <div className='project-similar'>
+                        <div>
+                            <div className='project-simi-title'>Similar Rooms</div>
+
+                            <div className='project-detail'>
+                                <Slider {...settings2}>
+                                    {ProjectsDataSimilar.map((prjsimi) => (
+                                        <div key={prjsimi.id}>
+                                            <div className='column'>
+                                                <div className='card-detail'>
+                                                    <div className='img-detail'>
+                                                        <img src={prjsimi.img} alt={prjsimi.name} />
+                                                    </div>
+                                                    <div className='project-list-detail'>
+                                                        <div className='project-list-title'>
+                                                            <h3 className='project-list-name'>{prjsimi.name}</h3>
+                                                            <h3 className='project-list-feedback'><FontAwesomeIcon icon={faStar} color='#FFD43B' />{prjsimi.feedback}</h3>
+
+                                                        </div>
+                                                        <h4>{prjsimi.adr}</h4>
+                                                        <div className='project-list-cost'>
+                                                            ${prjsimi.cost} <a>/ night</a>
+                                                        </div>
+                                                    </div>
+                                                    <p>
+                                                        <Link to={`detail/${prjsimi.id}`}>
+                                                            <button
+                                                                onClick={() => handleProjectClick(prjsimi)}
+                                                                className='project-list-button-view'
+                                                            >
+                                                                <a className='project-list-view'>View</a>
+                                                            </button>
+                                                        </Link>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </Slider>
+
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <div className='room-title'>Best Room</div>
+                            <div className='room-item'>
+
+                                {RoomData.map((room) => (
+
+                                    <div key={room.id}>
+                                        <div className='room-card'>
+                                            <div className='img-room'>
+                                                <img src={room.imgRoom} alt={room.titleRoom} />
+
+                                            </div>
+                                            <div className='room-detail'>
+                                                <h2>{room.titleRoom}</h2>
+                                                <p>${room.costRoom}</p>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                ))}
+
+
+
+                            </div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </div>
+
     )
+
+
+
 }
