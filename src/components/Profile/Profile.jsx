a379cc286b988a003b14102396688ccda0073215
duyluongg <147598253+duyluongg@users.noreply.github.com>
2024-03-09 00:59:48 +0700

    fix profile css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import axios from 'axios';
+import { format } from 'date-fns';
 
 export default function Profile({ getData }) {
     console.log(getData);
@@ -16,7 +17,7 @@ export default function Profile({ getData }) {
             fetchDataUser(getData);
         }
     }, [getData]);
-    
+
     const [accountUser, setAccountUser] = useState();
     const fetchDataUser = async (getData) => {
         console.log(getData);
@@ -27,7 +28,20 @@ export default function Profile({ getData }) {
 
             ]);
 
-            setAccountUser(accountResponse.data);
+            // const formattedBirthday = new Date(accountResponse.data.accBirthday);
+            // const dd = String(formattedBirthday.getDate()).padStart(2, '0');
+            // const mm = String(formattedBirthday.getMonth() + 1).padStart(2, '0');
+            // const yyyy = formattedBirthday.getFullYear();
+            // const formattedDate = `${dd}/${mm}/${yyyy}`;
+
+          
+            // const formattedData = { ...accountResponse.data, accBirthday: formattedDate };
+
+            const formattedBirthday = format(new Date(accountResponse.data.accBirthday), 'dd/MM/yyyy');
+
+          
+            const formattedData = { ...accountResponse.data, accBirthday: formattedBirthday };
+            setAccountUser(formattedData);
             console.log(accountResponse.data);
 
         } catch (error) {
@@ -37,70 +51,67 @@ export default function Profile({ getData }) {
     };
 
     return (
-        // <div>
 
 
-        //     <div className='profile-flex'>
-        //         <div className='profile-left'>
-        //             <Avatar
-        //                 alt="Remy Sharp"
-        //                 src="/static/images/avatar/1.jpg"
-        //                 sx={{ width: 96, height: 96 }}
-        //             />
-        //             <h2>{accountUser.accName}</h2>
-
-        //         </div>
-        //         <div className='profile-right-big'>
-        //             <div>
-        //                 <h2 className='profile-title-info'>Information</h2>
-        //                 <div className='profile-right'>
-        //                     <div>
-        //                         <h2 className='profile-title'>Email:</h2>
-        //                         <p> {accountUser.accEmail}</p>
-        //                         <h2 className='profile-title'>Phone:</h2>
-        //                         <p>{accountUser.accPhone}</p>
-        //                     </div>
-        //                     <div>
-        //                         <h2 className='profile-title'>Username:</h2>
-        //                         <p>{accountUser.accBirthday}</p>
-        //                         <h2 className='profile-title'>Location:</h2>
-        //                         <p>{accountUser.accBirthday}</p>
-
-        //                     </div>
-        //                 </div>
-        //             </div>
-
-        //             <div>
-        //                 <h2 className='profile-title-info'>Project</h2>
-        //                 <div className='profile-right'>
-        //                     <div>
-        //                         <h2 className='profile-title'>Email:</h2>
-        //                         <p> {accountUser.accEmail}</p>
-        //                         <h2 className='profile-title'>Phone:</h2>
-        //                         <p>{accountUser.accPhone}</p>
-        //                     </div>
-        //                     <div>
-        //                         <h2 className='profile-title'>Username:</h2>
-        //                         <p>{accountUser.roleID}</p>
-        //                         <h2 className='profile-title'>Location:</h2>
-        //                         <p>{accountUser.accStatus}</p>
-
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-
-
-        //     </div>
-        // </div>
-        // <div>{accountUser}</div>
         <div>
-        {accountUser && (
-            // Hiển thị dữ liệu ở đây
-            <div>{accountUser.accName}</div>
-        )}
-    </div>
+            {accountUser && (
+
+                <div>
+
+                    <div className='profile-flex'>
+                        <div className='profile-left'>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="/static/images/avatar/1.jpg"
+                                sx={{ width: 96, height: 96 , mt: "40px", mb: "20px"}}
+                            />
+                            <h2>{accountUser.accName}</h2>
+
+                        </div>
+                        <div className='profile-right-big'>
+                            <div>
+                                <h2 className='profile-title-info'>Information</h2>
+                                <div className='profile-right'>
+                                    <div>
+                                        <h2 className='profile-title'>Email:</h2>
+                                        <p> {accountUser.accEmail}</p>
+                                        <h2 className='profile-title'>Phone:</h2>
+                                        <p>{accountUser.accPhone}</p>
+                                    </div>
+                                    <div>
+                                        <h2 className='profile-title'>Username:</h2>
+                                        <p>{accountUser.accBirthday}</p>
+                                        <h2 className='profile-title'>Location:</h2>
+                                        <p>{accountUser.accBirthday}</p>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <h2 className='profile-title-info'>Project</h2>
+                                <div className='profile-right'>
+                                    <div>
+                                        <h2 className='profile-title'>Email:</h2>
+                                        <p> {accountUser.accEmail}</p>
+                                        <h2 className='profile-title'>Phone:</h2>
+                                        <p>{accountUser.accPhone}</p>
+                                    </div>
+                                    <div>
+                                        <h2 className='profile-title'>Username:</h2>
+                                        <p>{accountUser.roleID}</p>
+                                        <h2 className='profile-title'>Location:</h2>
+                                        <p>{accountUser.accStatus}</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
 
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
