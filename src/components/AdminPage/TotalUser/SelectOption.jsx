61771f130d54c954fb40afb2705f2ae5a1b97fd8
duyluongg <147598253+duyluongg@users.noreply.github.com>
2024-03-10 21:21:54 +0700

    fix change role

--- src/components/AdminPage/TotalUser/SelectOption.jsx
+++ src/components/AdminPage/TotalUser/SelectOption.jsx
@@ -5,29 +5,35 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function SelectOption({ row, onRole }) {
+    // const [selectedRole, setSelectedRole] = useState(row.status);
+
+    // const handleRoleChange = (event) => {
+    //     const newRole = event.target.value;
+    //     onRole(row, newRole);
+    //     setSelectedRole(newRole); 
+    // };
+
     const [selectedRole, setSelectedRole] = useState(row.status);
 
     const handleRoleChange = (event) => {
         const newRole = event.target.value;
         onRole(row, newRole);
-        setSelectedRole(newRole); 
+        setSelectedRole(newRole);
     };
 
     return (
-        <div>
-            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id={`role-label-${row.id}`}>{selectedRole}</InputLabel>
-                <Select
-                    labelId={`role-label-${row.id}`}
-                    id={`role-select-${row.id}`}
-                    value={selectedRole}
-                    onChange={handleRoleChange}
-                    label="Role"
-                >
-                    <MenuItem value="active">Active</MenuItem>
-                    <MenuItem value="block">Block</MenuItem>
-                </Select>
-            </FormControl>
-        </div>
+        <FormControl sx={{ width: "100px" }}>
+            <InputLabel id="demo-simple-select-label">{selectedRole}</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedRole}
+                label="Age"
+                onChange={handleRoleChange}
+            >
+                <MenuItem value="active">Active</MenuItem>
+                <MenuItem value="block">Block</MenuItem>
+            </Select>
+        </FormControl>
     );
 }
